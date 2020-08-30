d408a54b24eec978b9dafae6d0b35c959edeabe7
Daniel Phan <danielphan.2003@gmail.com>
2020-08-30 15:53:44 +0700

    add onEdit functionality

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,8 @@
-import * as publicUiFunctions from './ui';
-import * as publicSheetFunctions from './sheets';
+import * as publicUiFunctions from './sheets/context/ui';
+import * as publicSheetFunctions from './sheets/context/sheets';
 
 // Expose public functions by attaching to `global`
 global.onOpen = publicUiFunctions.onOpen;
 global.openDialog = publicUiFunctions.openDialog;
 global.openSidebar = publicUiFunctions.openSidebar;
-
+global.onEdit = publicSheetFunctions.onEdit;
