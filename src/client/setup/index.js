1e15a523c1050fa8d9f9308d702ed762a94d3537
Daniel Phan <danielphan.2003@gmail.com>
2020-08-30 15:48:02 +0700

    removed duplicated client interface

--- src/client/setup/index.js
+++
@@ -1,8 +0,0 @@
-import React, { render } from 'preact';
-import SheetEditor from './components/SheetEditor';
-
-import './styles.css';
-
-if (module.hot) require('preact/debug')
-
-render(<SheetEditor />, document.getElementById('index'));
--- src/client/sidebar/index.js
+++
@@ -1,6 +0,0 @@
-import React, { render } from 'preact';
-import App from './components/App';
-
-if (module.hot) require('preact/debug')
-
-render(<App />, document.getElementById('index'));
